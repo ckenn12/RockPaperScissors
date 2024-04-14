106c303b6f90e6b103535fd9969ad03ef12bb8d3
Caleb <calebkenyonpdx@gmail.com>
2024-04-14 13:38:14 -0600

    Add a UI and enable play with buttons, not input

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+buttons = document.querySelector("#buttons")
+rock = document.querySelector(".rock")
+paper = document.querySelector(".paper")
+scissors = document.querySelector(".scissors")
+
+yourScore = parseInt(document.querySelector(".yourScore").textContent)
+computerScore = parseInt(document.querySelector(".computerScore").textContent)
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"]
     let computerChoiceIndex = Math.floor(Math.random()*3)
@@ -6,34 +14,47 @@ function getComputerChoice(){
 }
 
 function playGameRound(computerSelection, playerSelection){
-    if(computerSelection === "rock" && playerSelection.toLowerCase() === "scissors"){
-        return "You lose. Rock beats scissors."
+    if(computerSelection === "rock" && playerSelection === "scissors"){
+        computerScore++
     }
-    else if(computerSelection === "rock" && playerSelection.toLowerCase() === "paper"){
-        return "You win. Paper beats rock."
+    else if(computerSelection === "rock" && playerSelection === "paper"){
+        yourScore++
     }
-    else if(computerSelection === "paper" && playerSelection.toLowerCase() === "rock"){
-        return "You lose. Paper beats rock."
+    else if(computerSelection === "paper" && playerSelection === "rock"){
+        computerScore++
     }
-    else if(computerSelection === "paper" && playerSelection.toLowerCase() === "scissors"){
-        return "You win. Scissors beats paper."
+    else if(computerSelection === "paper" && playerSelection === "scissors"){
+        yourScore++
     }
-    else if(computerSelection === "scissors" && playerSelection.toLowerCase() === "paper"){
-        return "You lose. Scissors beats paper."
+    else if(computerSelection === "scissors" && playerSelection === "paper"){
+        computerScore++
     }
-    else if(computerSelection === "scissors" && playerSelection.toLowerCase() === "rock"){
-        return "You win. Rock beats scissors"
+    else if(computerSelection === "scissors" && playerSelection === "rock"){
+        yourScore++
     }
     else{
-        return "It's a tie."
+        //tie
+    }
+    document.querySelector(".yourScore").textContent = yourScore
+    document.querySelector(".computerScore").textContent = computerScore
+    document.querySelector(".compChoice").textContent = `Your choice: ${playerSelection}. Computer choice: ${computerSelection}`
+    if(yourScore >= 5) {
+        alert("You are the winner!")
+    }
+    if(computerScore >= 5) {
+        alert("The computer beat you.")
     }
 }
 
+
+buttons.addEventListener('click', (e) => {
+    playerSelection = e.target.textContent.toLowerCase()
+    console.log(playerSelection)
+    playGame()
+})
+
 function playGame(){
-    return playGameRound(getComputerChoice(), prompt("What is your move: rock, paper, or scissors?"))
+    return playGameRound(getComputerChoice(), playerSelection)
 }
 
 
-for(let i=0; i<5; i++){
-    console.log(playGame())
-}
